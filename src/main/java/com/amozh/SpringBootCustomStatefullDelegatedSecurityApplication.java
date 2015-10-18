14a61cb4e254648f7d2bf2d746039fc90b07458c
Bubl1c <andrii.mozharovskyi@gmail.com>
2015-10-18 22:04:31 +0300

    Initial commit

---
+++ src/main/java/com/amozh/SpringBootCustomStatefullDelegatedSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.amozh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomStatefullDelegatedSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCustomStatefullDelegatedSecurityApplication.class, args);
+    }
+}
